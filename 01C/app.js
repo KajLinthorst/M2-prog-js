ed1f46575223acb41d34656d0b32ea665cbe3044
Kaj <37048@ma-web.nl>
2023-11-24 09:41:38 +0100

    02 canvashuis geadd

--- 01C/app.js
+++ 01C/app.js
@@ -5,7 +5,7 @@ class App
         let boolean = true;
         let string = "ik hou van appels";
         let number = 5;
-
+       
         let appNaam = "Kaj's coole app";
         let appVersie = "1.0";
         let appVersieDate = "20 November 2023";
@@ -13,8 +13,8 @@ class App
         let appCopyright = "Kaj Copyright";
         let appDistribiteur = "Mezelf";
         let appDarkmode = false;
-
-
+   
+   
         console.log("boolean: " + boolean);
         console.log("string: " + string);
         console.log("Number: " + number)
@@ -26,12 +26,16 @@ class App
         console.log("App distrubiteur: " + appDistribiteur);
         console.log("darkmode: " + appDarkmode);
         this.greeting = "Opgestart!";
+       
         this.boolean = false;
         this.number = 3;
         this.string = "Ik hou van meloen"
 
 
     }
+
+    
+
 }
 
 let app = new App();
