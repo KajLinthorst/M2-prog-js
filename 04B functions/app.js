c96e53d626ae2a1834b50fa71bb9b5094a91b5bb
Kaj <37048@ma-web.nl>
2023-12-01 10:09:47 +0100

    functions opdracht b toegevoegd

---
+++ 04B functions/app.js
@@ -0,0 +1,30 @@
+function superMooieGlobalFunction()
+{
+
+    console.log("ik ben globaal")
+    console.log("dus je mag mij overal aanroepen")
+
+}
+/* 
+superMooieGlobalFunction();
+superMooieGlobalFunction();
+superMooieGlobalFunction();
+*/
+function argumentsAreHandy(shoutout)
+{
+    console.log("do you want to give a shoutout?")
+    console.log(shoutout)
+}
+argumentsAreHandy("Super Shout out");
+argumentsAreHandy("Ik hou van appels");
+argumentsAreHandy("Hou het netjes");
+class App
+{
+    runApplication(){
+       //code hier
+        
+    }
+}
+
+let app = new App();
+app.runApplication();
