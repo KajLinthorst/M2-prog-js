49b90506ab2c7ea2c841b0499a28579dfb2a7d40
Kaj <37048@ma-web.nl>
2023-12-01 10:03:14 +0100

    04 toegevoegd

---
+++ 04 functions/app.js
@@ -0,0 +1,22 @@
+function superMooieGlobalFunction()
+{
+
+    console.log("ik ben globaal")
+    console.log("dus je mag mij overal aanroepen")
+
+}
+superMooieGlobalFunction();
+superMooieGlobalFunction();
+superMooieGlobalFunction();
+
+
+class App
+{
+    runApplication(){
+       //code hier
+        
+    }
+}
+
+let app = new App();
+app.runApplication();
