47ee97daed42a0ae147f4de5a8d623576ffd0f38
Kaj <37048@ma-web.nl>
2024-01-19 09:30:26 +0100

    7B

---
+++ 07B Functions/app.js
@@ -0,0 +1,44 @@
+class App {
+
+    
+    runApplication() {
+                  
+        }
+
+    }
+
+class Greet {
+    constructor(){
+        
+    }
+
+    showGreeting(){
+        console.log("Greetings!");
+    }
+}
+
+class GoodBye{
+    constructor(){
+        
+    }
+
+    showGoodbye(){
+        console.log("Bye Bye!!");
+    }
+}
+
+let greet = new Greet()
+greet.showGreeting();
+let goodbye = new GoodBye();
+goodbye.showGoodbye();
+let app = new App();
+app.runApplication();
+
+greet.showGreeting();
+goodbye.showGoodbye();
+
+greet.showGreeting();
+goodbye.showGoodbye();
+
+greet.showGreeting();
+goodbye.showGoodbye();
