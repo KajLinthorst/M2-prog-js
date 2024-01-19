437a58c5935081a632b5f50538b4e1cd3c2e4dae
Kaj <37048@ma-web.nl>
2024-01-19 09:23:30 +0100

    Paar opdrahten toegevoegd

---
+++ 07A Class/app.js
@@ -0,0 +1,27 @@
+class App {
+
+    
+    runApplication() {
+                  
+        }
+
+    }
+
+class Greet {
+    constructor(){
+        console.log("Greetings!");
+    }
+}
+
+class GoodBye{
+    constructor(){
+        console.log("Bye Bye!")
+    }
+}
+
+let greet = new Greet()
+new Greet()
+new Greet()
+let goodbye = new GoodBye();
+let app = new App();
+app.runApplication();
