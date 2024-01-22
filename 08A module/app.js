9964a3c419aaf2d77934357cdad5bca8e95fb4a7
KajLinthorst <37048@ma-web.nl>
2024-01-22 14:11:53 +0100

    08a

---
+++ 08A module/app.js
@@ -0,0 +1,17 @@
+import {Dino} from "./dino.mjs"
+import {Plant} from "./plant.mjs"
+
+class App
+{
+    
+    runApplication()
+    {
+    
+    }
+}
+let app = new App();
+app.runApplication();
+let dino = new Dino("moduliostaurus")
+let plant = new Plant("Plantkes")
+console.log(plant)
+console.log(dino)
